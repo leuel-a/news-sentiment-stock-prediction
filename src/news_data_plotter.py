8ed30e571378a5e99dedbdd826021b3e43f75d67
leuel-a <leuela1993@gmail.com>
2024-08-30 20:38:06 +0300

    feat(task-1): do analysis of the publishers and publishing companies

--- src/news_data_plotter.py
+++ src/news_data_plotter.py
@@ -14,12 +14,19 @@ class NewsDataPlotter:
         self.data = data
 
     def plot_year_data(self, year: int, x_label: str, y_label: str):
+        # Year data in series format
         year_data = pd.Series(self.data.loc[year], index=[month[:3] for month in months_of_year])
+
+        # Plot the line chart
         year_data.plot(kind='line', figsize=(20, 10), marker='o', lw='2')
 
         plt.xticks(ticks=np.arange(0, 12, 1), labels=[month[:3] for month in months_of_year])
         plt.yticks(ticks=np.arange(0, year_data.max() + 5, step=2))
 
-        plt.savefig(f"./images/{year}_News_Month_News_Count_Plot", format='png')
+        plt.title(f"News Count for Year {year}")
 
+        # add the labels for the grid
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
 
+        plt.grid(True)
