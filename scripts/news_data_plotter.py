d71fb30497d687986d2ab1e41633bcb030203ab9
leuel-a <leuela1993@gmail.com>
2024-09-03 22:41:58 +0300

    feat(task-2): finish the third task

--- scripts/news_data_plotter.py
+++ scripts/news_data_plotter.py
@@ -1,32 +1,20 @@
+from typing import Any
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from typing import Any
-
-months_of_year = [
-    "January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"
-]
 
 
 class NewsDataPlotter:
-    def __init__(self, data: Any):
-        self.data = data
+    _months_of_year = [
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ]
 
-    def plot_year_data(self, year: int, x_label: str, y_label: str):
-        # Year data in series format
-        year_data = pd.Series(self.data.loc[year], index=[month[:3] for month in months_of_year])
+    @staticmethod
+    def plot_monthly_data(data: pd.DataFrame, year: int, month: int) -> None:
+        pass
 
-        # Plot the line chart
-        year_data.plot(kind='line', figsize=(20, 10), marker='o', lw='2')
-
-        plt.xticks(ticks=np.arange(0, 12, 1), labels=[month[:3] for month in months_of_year])
-        plt.yticks(ticks=np.arange(0, year_data.max() + 5, step=2))
-
-        plt.title(f"News Count for Year {year}")
-
-        # add the labels for the grid
-        plt.xlabel(x_label)
-        plt.ylabel(y_label)
-
-        plt.grid(True)
+    @staticmethod
+    def plot_yearly_data(data: pd.DataFrame, year: int):
+        pass
